044b08c92aee3d25304b988196f16c0dea142c49
Deep Bhatt <bhattdeep38@gmail.com>
2023-12-05 12:22:46 +0530

    implement add to cart feature

--- models/models.py
+++ models/models.py
@@ -4,9 +4,24 @@ class Item:
     self.name = name
     self.price = price
 
+  def to_dict(self):
+    """ Convert the object to a dictionary """
+    return {
+        "id": self.id,
+        "name": self.name,
+        "price": self.price
+    }
+
 class Cart:
   def __init__(self):
     self.items = []
+
+  def to_dict(self):
+    """ Convert the object to a dictionary """
+    cart = []
+    for item in self.items:
+      cart.append(item.to_dict())
+    return cart
 
 class Order:
   def __init__(self):
--- routes/cart.py
+++ routes/cart.py
@@ -2,11 +2,16 @@ from flask import request, jsonify
 from models.models import Item, Cart, Order
 
 def cart_routes(app, cart: Cart):
-
   @app.route('/cart/add', methods=['POST'])
   def add_to_cart():
-    # add items to cart
-    return jsonify({'msg': 'to be implemented'}), 501
+    data = request.json
+    item = Item(data['id'], data['name'], data['price'])
+    cart.items.append(item)
+
+    return jsonify(
+      {"message": "Item added to cart"},
+      {"cart": cart.to_dict()}
+    ), 200
 
   @app.route('/checkout', methods=['POST'])
   def checkout():

4ac5d48f0415689f6011087f0b95e2eb4cf41a1c
Deep Bhatt <bhattdeep38@gmail.com>
2023-12-05 12:59:17 +0530

    simplify way to access stats values globally

--- models/models.py
+++ models/models.py
@@ -28,16 +28,3 @@ class Order:
     self.items = []
     self.total = 0
     self.discount_code = None
-
-# Singleton Class
-class Stats:
-  _instance = None
-
-  def __new__(cls):
-    if cls._instance is None:
-      cls._instance = super(Stats, cls).__new__(cls)
-      cls._instance.item_purchased_count = {}
-      cls._instance.total_purchase_amount = 0
-      cls._instance.discount_codes_generated = []
-      cls._instance.total_discount_amount = 0
-    return cls._instance
---
+++ models/stats.py
@@ -0,0 +1,11 @@
+# An attempt to make attributes avaiable globally
+# For achieving 'Singleton'
+
+discount_codes_generated = []
+item_purchased_count = 0
+total_orders_placed = 0
+total_purchase_amount = 0
+total_discount_amount = 0
+
+nth_order_discount = 3
+valid_discount_codes = []

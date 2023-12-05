60d8af3e37c2fdc640e6aa82568509d43efa2043
Deep Bhatt <bhattdeep38@gmail.com>
2023-12-05 13:43:16 +0530

    have getters and setters for accessing global stats

--- models/stats.py
+++ models/stats.py
@@ -1,11 +1,58 @@
 # An attempt to make attributes avaiable globally
 # For achieving 'Singleton'
 
-discount_codes_generated = []
-item_purchased_count = 0
-total_orders_placed = 0
-total_purchase_amount = 0
-total_discount_amount = 0
+_discount_codes_generated = []
+_item_purchased_count = 0
+_total_orders_placed = 0
+_total_purchase_amount = 0
+_total_discount_amount = 0
+_valid_discount_codes = []
 
-nth_order_discount = 3
-valid_discount_codes = []
+# Discount code generated every 3rd order.
+_nth_order_discount = 3
+
+def get_discount_codes_generated():
+  return _discount_codes_generated
+
+def add_generated_discount_code(code):
+  _discount_codes_generated.append(code)
+
+def get_item_purchased_count():
+  return _item_purchased_count
+
+def increment_item_purchased_count(count):
+  global _item_purchased_count
+  _item_purchased_count += count
+
+def get_total_orders_placed():
+  return _total_orders_placed
+
+def increment_total_orders_placed():
+  global _total_orders_placed
+  _total_orders_placed += 1
+
+def get_total_purchase_amount():
+  return _total_purchase_amount
+
+def add_to_total_purchase_amount(amount):
+  global _total_purchase_amount
+  _total_purchase_amount += amount
+
+def get_total_discount_amount():
+  return _total_discount_amount
+
+def add_to_total_discount_amount(amount):
+  global _total_discount_amount
+  _total_discount_amount += amount
+
+def get_valid_discount_codes():
+    return _valid_discount_codes
+
+def add_valid_discount_code(code):
+  _valid_discount_codes.append(code)
+
+def remove_valid_discount_code(code):
+  _valid_discount_codes.remove(code)
+
+def nth_order_discount():
+  return _nth_order_discount

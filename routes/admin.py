a425bd01bbe3e9cffa1775214fbdf82b06b24921
Deep Bhatt <bhattdeep38@gmail.com>
2023-12-05 14:34:08 +0530

    implement admin stats route

--- routes/admin.py
+++ routes/admin.py
@@ -1,12 +1,15 @@
-from flask import request, jsonify
+from flask import jsonify
+from models.stats import *
 
 def admin_routes(app):
 
   @app.route('/stats', methods=['GET'])
   def get_statistics():
-    # return statistics such as:
-    # count of items purchased
-    # total purchase amount
-    # list of discount codes
-    # total discount amount
-    return jsonify({'msg': 'to be implemented'}), 501
+    resp = {
+      'items_purchased_count': get_item_purchased_count(),
+      'total_purchase_amount': get_total_purchase_amount(),
+      'discount_codes_generated': get_discount_codes_generated(),
+      'total_discount_availed': get_total_discount_amount(),
+    }
+
+    return jsonify(resp), 200

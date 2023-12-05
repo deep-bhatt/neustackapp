e9adcb3bbc66b764d7fb6cb3a8b7a54f8afc427b
Deep Bhatt <bhattdeep38@gmail.com>
2023-12-05 13:45:05 +0530

    implement checkout feature

--- routes/cart.py
+++ routes/cart.py
@@ -1,20 +1,21 @@
 from flask import request, jsonify
-from models.models import Item, Cart, Order
+from models.models import Item, Order, CartManager, Cart
 from services.discount import DiscountService
-from models.stats import discount_codes_generated, valid_discount_codes
+from models.stats import *
 
-def cart_routes(app, cart: Cart):
+def cart_routes(app, cart_manager: CartManager):
   @app.route('/cart/add', methods=['POST'])
   def add_to_cart():
     data = request.json
     item = Item(data['id'], data['name'], data['price'])
+    cart: Cart = cart_manager.get_cart()
     cart.items.append(item)
 
     code = ''
     if DiscountService.is_discount_applicable():
       code = DiscountService.generate_discount_code()
-      discount_codes_generated.append(code)
-      valid_discount_codes.append(code)
+      add_generated_discount_code(code)
+      add_valid_discount_code(code)
 
     resp = {
       "message": "Item added to cart",
@@ -28,11 +29,42 @@ def cart_routes(app, cart: Cart):
 
   @app.route('/checkout', methods=['POST'])
   def checkout():
+    data = request.json
+    cart = cart_manager.get_cart()
+
+    # if cart is empty. return error
+
+    code = ''
+    if data and data['discountCode']:
+      code = data['discountCode']
+
+    # validate discount code
+    if code and code not in get_valid_discount_codes():
+      return jsonify({'error': 'discount code invalid'}), 400
+
     # checkouts cart & creates an order
+    order = Order()
+    order.items = cart.items
+    order.total = sum(item.price for item in cart.items)
+    order.discount_code = code
 
-    # check if discount valid code exist
-    # if invalid, throw invalid code error
-    # if valid, apply 10% discount on total order price
-    # then remove discount code from the valid discount code list
+    if order.discount_code:
+      order.total_discount_availed = 0.1 * order.total
+      order.total = order.total - order.total_discount_availed
+      remove_valid_discount_code(order.discount_code)
 
-    return jsonify({'msg': 'to be implemented'}), 501
+    # Update stats
+    add_to_total_discount_amount(order.total_discount_availed)
+    add_to_total_purchase_amount(order.total)
+    increment_total_orders_placed()
+    increment_item_purchased_count(len(order.items))
+
+    # reset cart after checkout
+    cart_manager.reset_cart()
+
+    resp = {
+      'msg': 'Order placed successfully.',
+      'total_orders_placed': get_total_orders_placed()
+    }
+
+    return jsonify(resp), 201
